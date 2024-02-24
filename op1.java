75ed29bc7f94980d02b767c6fc80b755bb2a3310
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-02-24 10:28:03 +0530

    Create op1.java

---
+++ op1.java
@@ -0,0 +1,16 @@
+public class Main
+{
+	public static void main(String[] args) {
+		try{
+		    int a = 5/0	;	}
+	
+	    catch(Exception e)
+	    {
+	        catch(ArithmeticException a)
+	        {
+	            
+	        }
+	    }
+	    System.out.println("java");
+	}
+}
